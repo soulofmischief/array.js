cf3033785d45b7627090ad0a7eef79e7a62b2ffc
soulofmischief <soulofmischief@protonmail.com>
2020-10-03 17:23:32 -0500

    Add tests for empty objects in reduce functions

--- tests/reduce.js
+++ tests/reduce.js
@@ -5,25 +5,31 @@ import { byID, byName, byProp, toObject } from '../src/reduce'
 const
   obj1 = { name: 'test1', id: 't1', a: 1, b: 2 },
   obj2 = { name: 'test2', id: 't2', c: 4, d: 4 },
+  // Functions should handle empty object/array by returning an empty object.
+  obj3 = {},
   prop = 'id'
 
 
 o( 'toObject', () => {
   o( Object.entries( obj1 ).reduce( ...toObject())).deepEquals( obj1 )
   o( Object.entries( obj2 ).reduce( ...toObject())).deepEquals( obj2 )
+  o( Object.entries( obj3 ).reduce( ...toObject())).deepEquals( obj3 )
 })
 
 o( 'byName', () => {
   o([ obj1 ].reduce( ...byName())).deepEquals({[ obj1.name ]: obj1 })
   o([ obj2 ].reduce( ...byName())).deepEquals({[ obj2.name ]: obj2 })
+  o( Object.entries( obj3 ).reduce( ...toObject())).deepEquals( obj3 )
 })
 o( 'byID', () => {
   o([ obj1 ].reduce( ...byID())).deepEquals({[ obj1.id ]: obj1 })
   o([ obj2 ].reduce( ...byID())).deepEquals({[ obj2.id ]: obj2 })
+  o( Object.entries( obj3 ).reduce( ...toObject())).deepEquals( obj3 )
 })
 
 o( 'byProp', () => {
   o([ obj1 ].reduce( ...byProp( prop ))).deepEquals({[ obj1[ prop ]]: obj1 })
   o([ obj2 ].reduce( ...byProp( prop ))).deepEquals({[ obj2[ prop ]]: obj2 })
+  o( Object.entries( obj3 ).reduce( ...toObject())).deepEquals( obj3 )
 })
 
