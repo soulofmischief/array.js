eb86c2e90671c80182ee3497bc1d941eaae9f2a1
soulofmischief <soulofmischief@protonmail.com>
2020-10-04 21:52:00 -0500

    add byProp()

--- src/sort/index.js
+++ src/sort/index.js
@@ -1,6 +1,7 @@
 // @flow strict
-import type { ArraySort } from 'Flow'
 import { sort } from './sort'
+
+import type { ArraySort } from 'Flow'
 
 
 export { sort }
@@ -14,3 +15,17 @@ export const sortNumberDescending: ArraySort<*> = (
   arr: *[], prop?: *
 ) =>
   sort(( a, b ) => prop ? b[prop] - a[prop] : b - a, arr )
+
+export function byProp( prop, cb1, cb2 ) {
+  if ( cb1 && cb2 ) return ( a, b ) => {
+    if( cb1( a[ prop ], b[ prop ] )) return -1
+    if( cb2( a[ prop ], b[ prop ] )) return 1
+    return 0;
+  }
+
+  else return ( a, b ) => {
+    if( a[ prop ] < b[ prop ] ) return -1
+    if( a[ prop ] > b[ prop ] ) return 1
+    return 0;
+  }
+}
